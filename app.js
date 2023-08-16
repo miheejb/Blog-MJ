45ef350f672914ab2e0303de880411a715785fd0
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-08-17 10:40:46 +1200

    add express routing

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import _ from "lodash";
 
 
 
 const app = express();
+
+
 
 //Configure the view engine
 app.set("view engine", "ejs"); //Use EJS as the view engine
@@ -25,20 +28,57 @@ const aboutContent =
 const contactContent =
   "Quisque porta porttitor risus id pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus hendrerit auctor.";
 
+// global variable
+let posts =[];
+
+
 
 app.get("/", (req,res)=>{ 
 
-    
- res.render("home.ejs", {ejsContent: homeStartingContent});
+ res.render("home.ejs", {
+    startingContent: homeStartingContent,
+    posts:posts
+});
+
 });
 
 app.get("/about", (req,res)=>{
-    res.render("about.ejs", {ejContent: aboutContent})
+    res.render("about.ejs", {aboutContent: aboutContent})
 });
 
 app.get("/contact", (req,res)=>{
-    res.render("contact.ejs", {ejsContent: contactContent})
+    res.render("contact.ejs", {contactContent: contactContent})
 });
+
+app.get("/compose", (req, res) => {
+  res.render("compose.ejs");
+});
+
+
+
+app.post("/compose", (req,res)=>{
+    const post = {
+    title:req.body.postTitle,
+    postbody:req.body.postbody};
+
+    posts.push(post);
+    res.redirect("/");
+});
+
+app.get("/posts/:topic", (req,res)=>{
+
+    const requestedTitle = _.lowerCase(req.params.topic);
+    
+   posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+   if (requestedTitle === storedTitle){
+    console.log ("Match found");
+   } else {
+    console.log("Not a match");
+   };});
+});
+
 
 
 
